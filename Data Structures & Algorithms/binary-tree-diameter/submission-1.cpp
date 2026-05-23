c910b75495992a6f6cca956cc9360acb55da9da6
Dev Shinde <84491890+dev-shinde@users.noreply.github.com>
2026-05-23 16:29:50 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    int maxdepth = 0;
+
+    int dfs(TreeNode* root)
+    {
+        if (!root) return 0;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        maxdepth = max(maxdepth,left+right);
+        return 1 + max(left,right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return maxdepth;
+    }
+};
